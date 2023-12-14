a77ac83ef34be1fc82690e5293b8b4f81155718e
Thorsten Marx <t.marx@eggheads.de>
2023-12-14 08:57:43 +0100

    merge main

--- modules/ui-module/src/main/java/com/github/thmarx/cms/modules/ui/services/FileSystemService.java
+++ modules/ui-module/src/main/java/com/github/thmarx/cms/modules/ui/services/FileSystemService.java
@@ -6,20 +6,23 @@ package com.github.thmarx.cms.modules.ui.services;
  * %%
  * Copyright (C) 2023 Marx-Software
  * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  * 
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
 import com.github.thmarx.cms.api.ModuleFileSystem;
+import com.github.thmarx.cms.api.db.DB;
 import com.github.thmarx.cms.modules.ui.utils.PathUtil;
 import java.io.File;
 import java.io.IOException;
@@ -40,12 +43,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class FileSystemService {
 
-	private final ModuleFileSystem fileSystem;
+	private final DB db;
 
 	public List<Node> listContent(String parent) {
 
 		try {
-			var contentBase = fileSystem.resolve("content");
+			var contentBase = db.getFileSystem().resolve("content");
 			final Path parentNode = contentBase.resolve(parent);
 			if (!PathUtil.isChild(contentBase, parentNode)) {
 				return Collections.emptyList();
@@ -62,7 +65,7 @@ public class FileSystemService {
 	}
 
 	public boolean createFile(final String name, final String parent, final String content) throws IOException {
-		var contentBase = fileSystem.resolve("content");
+		var contentBase = db.getFileSystem().resolve("content");
 		final Path parentNode = contentBase.resolve(parent);
 		if (!PathUtil.isChild(contentBase, parentNode)) {
 			return false;
@@ -78,7 +81,7 @@ public class FileSystemService {
 	}
 
 	public String readFromFile(final String name) throws IOException {
-		var contentBase = fileSystem.resolve("content");
+		var contentBase = db.getFileSystem().resolve("content");
 		final Path file = contentBase.resolve(name);
 		if (!PathUtil.isChild(contentBase, file)) {
 			return "";
@@ -91,7 +94,7 @@ public class FileSystemService {
 	}
 	
 	public boolean writeToFile(final String name, final String content) throws IOException {
-		var contentBase = fileSystem.resolve("content");
+		var contentBase = db.getFileSystem().resolve("content");
 		final Path file = contentBase.resolve(name);
 		if (!PathUtil.isChild(contentBase, file)) {
 			return false;
@@ -104,7 +107,7 @@ public class FileSystemService {
 	}
 
 	public void delete(String path) throws IOException {
-		var contentBase = fileSystem.resolve("content");
+		var contentBase = db.getFileSystem().resolve("content");
 		var pathToBeDeleted = contentBase.resolve(path);
 		if (!Files.isDirectory(pathToBeDeleted)) {
 			Files.deleteIfExists(pathToBeDeleted);
@@ -116,7 +119,7 @@ public class FileSystemService {
 	}
 
 	public boolean createFolder(final String name, final String parent) throws IOException {
-		var contentBase = fileSystem.resolve("content");
+		var contentBase = db.getFileSystem().resolve("content");
 		final Path parentNode = contentBase.resolve(parent);
 		if (!PathUtil.isChild(contentBase, parentNode)) {
 			return false;
